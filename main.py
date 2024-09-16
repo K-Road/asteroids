cd555a9d50b71a418d7a601620bf996e8ef6543d
K-Road <game@chrodeprime.com>
2024-09-16 18:13:02 +1000

    added circleshape, player classes. added player movement

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
-    BLACK = (0,0,0)
+    #Sprite groups
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
 
-        screen.fill(BLACK)
+        for sprite in updatable:
+            sprite.update(dt)
+
+        screen.fill(BLACK)    
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        #framerate limit 60 fps
+        dt = clock.tick(60)/1000 #convert ms to sec
 
 
 if __name__ == "__main__":
